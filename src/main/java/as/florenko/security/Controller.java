a116cd0bd948ede3c59f47edebd8cb81211eaa7c
Alexandr Florenko <AlexandrFlor@yandex.ru>
2022-09-15 23:08:23 +0400

    Add Method security

--- src/main/java/as/florenko/security/Controller.java
+++ src/main/java/as/florenko/security/Controller.java
@@ -2,7 +2,12 @@ package as.florenko.security;
 
 import as.florenko.security.repository.Repository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/")
@@ -13,18 +18,27 @@ public class Controller {
         this.repository = repository;
     }
 
+    @Secured("ROLE_CITY")
     @GetMapping("/persons/by-city")
     public ResponseEntity<Object> getUserByCity(@RequestParam String city) {
         return repository.findByCity(city);
     }
 
+    @RolesAllowed("ROLE_AGE")
     @GetMapping("/persons/by-age")
     public ResponseEntity<Object> getUserByAge(@RequestParam int age) {
         return repository.findByAge(age);
     }
 
+    @PreAuthorize("hasRole('CITY') or hasRole('NAME')")
     @GetMapping("/persons/by-name")
     public ResponseEntity<Object> getUserByName(@RequestParam String name, String surname) {
         return repository.findByNameAdnSurname(name, surname);
+    }
+
+    @PostAuthorize("#username == authentication.principal.username")
+    @GetMapping("/persons/checkname")
+    public String getUserName(@RequestParam String username) {
+        return "Пользователь " + username + " подтвержден";
     }
 }
--- src/main/java/as/florenko/security/security/SecurityConfiguration.java
+++ src/main/java/as/florenko/security/security/SecurityConfiguration.java
@@ -1,32 +1,29 @@
 package as.florenko.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder a) throws Exception {
         a.inMemoryAuthentication()
-                .withUser("User").password("{noop}qwerty").authorities("age check")
+                .withUser("User").password(encoder().encode("qwerty")).roles("CITY")
                 .and()
-                .withUser("Qwer").password("{noop}asdfg").authorities("name check");
+                .withUser("Qwer").password(encoder().encode("asd")).roles("AGE")
+                .and()
+                .withUser("stas").password(encoder().encode("zxcvbn")).roles("NAME");
 
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("age check")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-name").hasAuthority("name check")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
     }
 }
